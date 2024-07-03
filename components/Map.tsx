412c87ee6329ff881482b56c1fd6c36e35cc69b6
Hafiz Toyyib <hafizthesakora@gmail.com>
2024-07-03 17:33:49 +0000

    Adding markers

--- components/Map.tsx
+++ components/Map.tsx
@@ -1,14 +1,39 @@
-import Mapbox, { Camera, LocationPuck, MapView } from '@rnmapbox/maps';
+import Mapbox, {
+  Camera,
+  Images,
+  LocationPuck,
+  MapView,
+  ShapeSource,
+  SymbolLayer,
+} from '@rnmapbox/maps';
+import { featureCollection, point } from '@turf/helpers';
 import { Text } from 'react-native';
-import { isEnabled } from 'react-native/Libraries/Performance/Systrace';
+
+import pin from '~/assets/pin.png';
+import scooters from '~/data/seeds.json';
 
 Mapbox.setAccessToken(process.env.EXPO_PUBLIC_MAPBOX_KEY || '');
 
 export default function Map() {
+  const points = scooters.map((scooter) => point([scooter.long, scooter.lat]));
+  const scooterFeatures = featureCollection(points);
+
   return (
     <MapView style={{ flex: 1 }} styleURL="mapbox://styles/mapbox/dark-v11">
       <Camera followZoomLevel={16} followUserLocation />
       <LocationPuck puckBearingEnabled puckBearing="heading" pulsing={{ isEnabled: true }} />
+
+      <ShapeSource id="scooters" shape={scooterFeatures}>
+        <SymbolLayer
+          id="scooter-icons"
+          style={{
+            iconImage: 'pin',
+            iconSize: 0.5,
+            iconAllowOverlap: true,
+          }}
+        />
+        <Images images={{ pin }} />
+      </ShapeSource>
     </MapView>
   );
 }
